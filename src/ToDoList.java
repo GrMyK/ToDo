338e7ffa65006cffb4567b8462edae93f22a0b34
GrMyk <kulemetos@gmail.com>
2019-05-24 17:31:15 +0300

    todo for exersise

---
+++ src/ToDoList.java
@@ -0,0 +1,10 @@
+/** my trying**/
+public class ToDoList {
+    public static void main(String[] args) {
+        System.out.println ("tusk1 - weakeup");
+            System.out.println("tusk2 - work");
+                 System.out.println("tusk3 - sleep");
+        }
+    }
+
+
